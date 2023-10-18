a2c44a5edad5b433e528b25529119ecc1fbd11d5
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2023-10-18 16:09:19 +0530

    base files of Front-End

---
+++ Front-End/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BookStore = () => {
+  return (
+    <div className="bg-red-500 font-bold text-center" style={{ padding: '1.2rem', fontSize: '1.7rem', fontFamily: 'NewFont' }}>BookStore</div>
+
+  )
+}
+
+export default BookStore
