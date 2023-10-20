e377044aa56eb2d19b20c99501b73ebab63e7dda
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2023-10-20 17:25:25 +0530

    Set SPA and routeDOM

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
+import home from './pages/home.jsx';
+import createBook from './pages/createBook.jsx';
+import deleteBook from './pages/deleteBook.jsx';
+import fetchBook from './pages/fetchBook.jsx';
+import updateBook from './pages/updateBook.jsx';
 
 const BookStore = () => {
   return (
     // <div className="bg-red-500 font-bold text-center" style={{ padding: '1.2rem', fontSize: '1.7rem', fontFamily: 'NewFont' }}>BookStore</div>
     <Routes>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
+      <Route path='/' element={home}/>
+      <Route path='/books/create' element={createBook}/>
+      <Route path='/books/delete/:id' element={deleteBook}/>
+      <Route path='/books/update/:id' element={updateBook}/>
+      <Route path='/books/detail/:id' element={fetchBook}/>
     </Routes>
 
   )
